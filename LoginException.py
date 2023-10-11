3062b1dcb8cba5a2cbcca0bf826da90c22b2aad1
mamangbili <legendaryma30@gmail.com>
2023-10-11 15:22:13 +0700

    buat exception login

---
+++ LoginException.py
@@ -0,0 +1,23 @@
+class LoginWrongPasswordException(Exception):
+    def __init__(self,message):
+        super().__init__(message)
+        self.message = message
+        
+    def __repr__(self) -> str:
+        return super().__repr__()
+
+    
+class LoginWrongIdException(Exception):
+    def __init__(self,message):
+        super().__init__(message)
+        
+    def __repr__(self) -> str:  
+        return super().__repr__()
+        
+class LoginWrongIdAndPasswordException(Exception):
+    def __init__(self,message):
+        super().__init__(message)
+
+    def __repr__(self) -> str:
+        return super().__repr__()
+
