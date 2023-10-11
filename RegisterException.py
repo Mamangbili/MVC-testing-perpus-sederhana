18a6aaf1c42617e1a44326b07651192cdbf56b3c
mamangbili <legendaryma30@gmail.com>
2023-10-11 15:22:38 +0700

    buat exception register

---
+++ RegisterException.py
@@ -0,0 +1,16 @@
+
+class RegisterIdAlreadyExistException(Exception):
+    def __init__(self,message): 
+        super().__init__(message)
+    
+    def __repr__():
+        return super().__repr__()
+
+class RegisterPasswordNotMatchException(Exception):
+    def __init__(self,message): 
+        super().__init__(message)
+    
+    def __repr__():
+        return super().__repr__()
+
+
